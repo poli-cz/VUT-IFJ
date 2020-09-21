1c7d4932019e7ee77715fb338bd0f5c938562e54
PoliĹĄenskĂ˝ Jan <xpolis04@stud.fit.vutbr.cz>
2020-09-21 18:03:48 +0200

    some sort of change

--- src/common.c
+++ src/common.c
@@ -2,22 +2,4 @@
 
 #include <common.h>
 // safe alloc and malloc
-
-void *safeMalloc(size_t _Size){
-	void *ret = malloc(_Size);
-	if(ret == NULL){
-		fprintf(stderr, "[INTERNAL] Fatal error - cannot allocate memory\n");
-		exit(99);
-	}
-	return ret;
-}
-
-
-void *safeRealloc(void *_Block, size_t _Size){
-	void *ret = realloc(_Block, _Size);
-	if(ret == NULL){
-		fprintf(stderr, "[INTERNAL] Fatal error - cannot allocate memory\n");
-		exit(99);
-	}
-	return ret;
-}
+// common stuf for all 
