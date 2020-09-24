e822455317c37d4ff422d117ae74449de6ea4236
PoliĹĄenskĂ˝ Jan <xpolis04@stud.fit.vutbr.cz>
2020-09-24 12:12:52 +0200

    structures works for now

---
+++ src/scanner.c
@@ -0,0 +1,35 @@
+/*
+ * Předmět  :   IFJ / IAL
+ * Soubor   :   scanner.c - lexikální analyzátor reprezentovaný konečným automatem
+ * Projekt  :   Implementace překladače imperativního jazyka IFJ20
+ * Tým č    :
+ * Varianta :
+ * Autor    : Jan Polišenský (xpolis04)
+ */
+
+#include <stdio.h>
+#include "scanner.h"
+
+lex_token get_token(){
+
+  lex_token token;
+  char c; // curent char from stdin
+
+  while(1){
+    c = getchar();
+
+
+    token.value = c;
+    return(token);
+  }
+
+
+
+
+
+
+
+
+
+   return(token);
+}
