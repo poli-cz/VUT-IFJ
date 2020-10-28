d40a642fcc9cd35aa602779fa4a71b96a5e2b836
Ninka <ninkastefek@gmail.com>
2020-10-28 01:15:28 +0100

    update repo

---
+++ src/dynamic_string.h
@@ -0,0 +1,39 @@
+/*
+ * Předmět  :   IFJ / IAL
+ * Soubor   :   scanner.c - lexikální analyzátor reprezentovaný konečným automatem
+ * Projekt  :   Implementace překladače imperativního jazyka IFJ20
+ * Tým č    :
+ * Varianta :
+ * Autor    : 
+ */
+#ifndef DYNAMIC_STRING_H
+#define DYNAMIC_STRING_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* Initial size of array
+*/
+#define SIZE_ARR 8
+/**
+  * structure of dynamic array
+  * *str - string,
+  * length - length of string,
+  * allocated - allocated memory for string
+*/
+typedef struct {
+    char* str;
+    unsigned int len;
+    unsigned int alloc;
+} dynamic_string;
+
+/**
+  *Initialise memory
+*/
+int init_dynamic_string(dynamic_string **string);
+
+/**
+* Adds char at the end of array
+*/
+int add_char(char c,dynamic_string *string);
+
+#endif
--- src/scanner.h
+++ src/scanner.h
@@ -8,6 +8,11 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "dynamic_string.h"
+#define KEYWORD_NUM 18
+
+
 
 typedef enum{
   t_id,
@@ -15,32 +20,78 @@ typedef enum{
   t_if,
   t_and,
   t_or,
+  t_eol,
+  t_mul,
+  t_div,
+  t_plus,
+  t_minus,
+  t_comma,
+  t_semico,
+  t_eq,
+  t_assign,
+  t_grt,
+  t_lesst,
+  t_lbra,
+  t_rbra,
+  t_colon,
+  t_curll,
+  t_curlr,
+  t_exclam,
+  t_neq,
+  t_grteq,
+  t_lste,
+  t_undefined
   // atd...
 
 }token_type;
 
 // definice stavů lexikálního analyzátoru
 typedef enum{
-  start,
-  key_word,
-  integer,
-  string,
+  s_error,
+  s_start,
+  s_id,
+  s_key_word,
+  s_number,
+  s_float,
+  s_exp,
+  s_endcom,
+  s_blockcom,
+  s_string,
+  s_eof,
+  s_eol,
+  s_mul,
+  s_div,
+  s_lslash,
+  s_plus,
+  s_minus,
+  s_comma,
+  s_semico,
+  s_eq,
+  s_assign,
+  s_grt,
+  s_lst,
+  s_lbra,
+  s_rbra,
+  s_colon,
+  s_curll,
+  s_curlr,
+  s_exclam,
+  s_fact,
+  s_linecom
+
   // TODO add more states
 }lex_state;
 
 // definice struktury tokenu posílaného z lexikálního analyzátoru
 typedef struct{
-    int value; // value of token
+    dynamic_string* value; // value of token
     int line;   // position of token
     int coll;
     token_type type;
-}lex_token;
-
-
+}tToken;
 
 
 // definition of used functions
-
-
+token_type get_token();
 
 int parser();
