1cfa8de9521035ce08ce293558415c38ee8e4ff9
poli-cz <polisensky.jan@seznam.cz>
2020-11-19 22:48:13 +0100

    interpret

---
+++ src/interpret/gen3.go
@@ -0,0 +1,23 @@
+// Program 2: Vypocet faktorialu (rekurzivne)
+package main
+
+
+
+func main() {
+  a := 40
+  b := 5*40 + a
+  print(b, "\n")
+  b = foo(a,b)
+  print(b, "\n")
+
+}
+
+
+func foo(a int, b int)(int){
+
+
+
+
+
+  return a*b
+}
